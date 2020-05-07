4e2804770016530b556df8af9f0852592a93bdb2
Kalyan <kalyankumarreddy098@gmail.com>
2020-05-07 19:01:07 +0530

    Angular

---
+++ src/app/auth-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
+import {environment} from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  login(data):Observable<any>{
+    return this.httpClient.post(environment.api+'/login',data);
+  }
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthServiceService } from '../auth-service.service';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  formGroup:FormGroup;
+  constructor(private authService:AuthServiceService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+  initForm(){
+    this.formGroup = new FormGroup({
+      username:new FormControl('',[Validators.required]),
+      password:new FormControl('',[Validators.required]),
+    })
+  }
+
+  loginProcess(){
+    if(this.formGroup.valid){
+      this.authService.login(this.formGroup.value).subscribe( result =>{
+        if(result.isAdmin){
+          this.router.navigate(['/admin']);
+         // console.log(result);
+         // alert("welcome Admin "+result.name);
+        }else{
+          alert("Welcome "+result.name);
+        }
+      },(error:any)=>alert("Invalid Username Or Password"))
+    }
+  }
+
+}
